dda4651f24532203cda04dbf95654b326f0cc367
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-03-01 09:23:46 -0800

    Completed 2-6

--- javascript/code-challenges/challenges-11.test.js
+++ javascript/code-challenges/challenges-11.test.js
@@ -67,7 +67,9 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
 const divisibleByFiveTwoToThePower = (input) => {
-  let second = [];
+  return input.map(row => {
+    return row.filter(cell => typeof cell === 'number' && cell% 5 === 0).map(num => Math.pow(2, num));
+  });
 
 };
 
@@ -134,7 +136,7 @@ let starWarsData = [{
 }];
 
 let findMaleAndFemale = (data) => {
- for(let )
+  return data.filter(char => char.gender === 'male' || char.gender === 'female').map(char => char.name).join(' and ');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -144,7 +146,7 @@ Write a function named findShortest that, given the Star Wars data from Challeng
 ------------------------------------------------------------------------------------------------ */
 
 let findShortest = (data) => {
-  // Solution code here...
+ return data.reduce((shortestNow, char) => +shortestNow.height < +char.height ? shortestNow : char).name;
 };
 
 /* ------------------------------------------------------------------------------------------------
